b2fe48168ca4d099325cea98d210b12fb59412f1
lucas <lucas_fernandez@orange.fr>
2021-04-12 13:20:22 +0200

    add features

--- parsing.py
+++ parsing.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 allfiles = [f for f in listdir("UMAFall_Dataset") if isfile(join("UMAFall_Dataset", f))]
 clean_file = []
-
+features = "TimeStamp; Sample No; X-Axis; Y-Axis; Z-Axis; Sensor Type; Sensor ID\n"
 for f in allfiles:
     file = open(join("UMAFall_Dataset", f), "r")
     lines = file.readlines()
     file.close()
-    output = []
+    output = [features]
     new_file = open(join("UMAFall_Dataset", f), "w+")
     for i in range(len(lines)):
         if lines[i].find("%") == 0 or len(lines[i].split()) == 0:
@@ -19,9 +19,9 @@ for f in allfiles:
             output.append(lines[i])
     new_file.writelines(output)
     new_file.close()
-
 """
-for f in allfiles:
+for f in allfiles[:2]:
     with open(join("UMAFall_Dataset", f)) as csv_file:
-        file = pd.read_csv(join("UMAFall_Dataset", f))
+        data = pd.read_csv(join("UMAFall_Dataset", f))
+        data[6] =
 """
