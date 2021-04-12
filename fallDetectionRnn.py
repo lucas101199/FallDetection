fcae21810cb20e5b94609d0560a0d90f8cc38305
lucas <lucas_fernandez@orange.fr>
2021-04-12 15:54:50 +0200

    test with simple NN

--- fallDetectionRnn.py
+++ fallDetectionRnn.py
@@ -1,22 +1,28 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import tensorflow_datasets as tfds
 from keras.layers import SimpleRNN
-
-Label = { 1:'fallen', 2: 'not_fallen'}
-
+from tensorflow.keras import layers
 
 
-data_train = pd.read_csv('UMAFall_Subject_01_ADL_Aplausing_1_2017-04-14_23-38-23.csv', sep = ';', header = 0)
-data_test = pd.read_csv('UMAFall_Subject_01_ADL_Aplausing_2_2017-04-14_23-38-59.csv', sep = ';', header = 0)
-train_dataset = dataset['data_train']
-test_dataset = dataset['sensor']
+Label = {0: 'fallen', 1: 'not_fallen'}
 
-#la ligne qui suive sert à limiter les test que sur les 10 premier lignes.
-pandas.options.display.max_rows = 10
+data_not = pd.read_csv('UMAFall_Dataset/UMAFall_Subject_01_ADL_Aplausing_1_2017-04-14_23-38-23.csv', sep = ';', header = 0)
+#data_fall = pd.read_csv("UMAFall_Dataset/UMAFall_Subject_04_Fall_forwardFall_6_2016-06-13_13-23-05.csv", sep=';', header=0)
+#data_test = pd.read_csv('UMAFall_Subject_01_ADL_Aplausing_2_2017-04-14_23-38-59.csv', sep = ';', header = 0)
+labels = []
+for i in range(len(data_not)):
+    if (i%2) == 0:
+        labels.append(1)
+    else:
+        labels.append(0)
 
-model = Sequential()
-model.add(SimpleRNN(units=16, input_shape=(10, 30), use_bias=False))
-model.fit(train_dataset, test_dataset)
+
+model = tf.keras.Sequential([
+    layers.Dense(64),
+    layers.Dense(1)
+])
+model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam())
+
+model.fit(np.array(data_not), np.array(labels), epochs=2)
 print(model.summary())
