164024515b42fe3daba7c72297e8295eae0dc1fc
sidathgueye <sidathgueye@gmail.com>
2021-04-14 09:42:01 +0200

    modification de falldetecton rnn

--- fallDetectionRnn.py
+++ fallDetectionRnn.py
@@ -1,3 +1,7 @@
+# Les import necessaires.
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
 import keras as keras
 import numpy as np
 import pandas as pd
@@ -6,45 +10,119 @@ import tensorflow as tf
 from tensorflow import keras
 from sklearn.preprocessing import StandardScaler
 
+def GetLabelFromFile(f, labels):
+    for k, v in labels.items():
+        if f.find(v) != -1:
+            return k
+    return -1
+
+# Récuperer tous les fichiers qu'il y'a dans le dossier "UMAFall_Dataset".
+allfiles = [f for f in listdir("UMAFall_Dataset") if isfile(join("UMAFall_Dataset", f))]
+
+allfiles
+
+# Préparer les features.
+features = "TimeStamp;Sample No;X-Axis;Y-Axis;Z-Axis;Sensor_Type;Sensor_ID\n"
+
+# pour chaque fichier qu'il y a dans le dossier "UMAFall_Dataset", lire ce fichier, 
+# et enregister son contenue dans un new_file, 
+# le but est de concaténer le contenue de tous les fichier qui se trouvent dans "UMAFall_Dataset".
+for f in allfiles:
+    lines = file.readlines()
+    file.close()
+    if lines[0] != features:
+        output = [features]
+        new_file = open(join("UMAFall_Dataset", f), "w+")
+        for i in range(len(lines)):
+            if lines[i].find("%") == 0 or len(lines[i].split()) == 0:
+                pass
+            else:
+                output.append(lines[i])
+        new_file.writelines(output)
+        new_file.close()
+
+# Préparation des différents Labels possible allant de 'Aplausing' à 'Fall'
+
+labels = {0: "Aplausing", 1: "HandsUp", 2: "MakingACall", 3: "OpeningDoor", 
+          4: "Sitting_GettingUpOnAChair", 5: "Walking", 6: "Bending", 7: "Hopping",
+          8: "Jogging", 9: "LyingDown", 10: "GoDownstairs", 11: "GoUpstairs", 12: "Fall"}
+
+features_action = features[:-1] + ';Action\n'
+allData = open('AllData.csv', 'w')
+allData.write(features_action)
+allData.close()
+
+# Enregistrer le contenu de tous les fichier qui se troyvent dans "UMAFall_Dataset" dans un seul dataset très grand nomé 'AllData.csv'
+
+for f in allfiles:
+    name_file = join("UMAFall_Dataset", f)
+    label = GetLabelFromFile(name_file, labels)
+    with open(name_file) as csv_file:
+        df = pd.read_csv(name_file, sep=';', header=0)
+        df = df[df.Sensor_ID == 3]
+        action = [label] * len(df.index)
+        df["Action"] = action
+        df.to_csv(r'AllData.csv', mode='a', header=False, index=False, sep=';')
+
+# Lire le dataset 'AllData.csv'
 
 Label = {0 : 'fallen', 1 : 'not_fallen'}
 
-data_not = pd.read_csv('UMAFall_Dataset/UMAFall_Subject_01_ADL_Aplausing_1_2017-04-14_23-38-23.csv', sep=';', header=0)
-# data_fall = pd.read_csv("UMAFall_Dataset/UMAFall_Subject_04_Fall_forwardFall_6_2016-06-13_13-23-05.csv", sep=';', header=0)
-# data_test = pd.read_csv('UMAFall_Subject_01_ADL_Aplausing_2_2017-04-14_23-38-59.csv', sep = ';', header = 0)
+data_not = pd.read_csv('AllData.csv', sep=';', header=0)
+
+data_not.shape
+
+# Préparation des targets pour ce dataset.
+
 labels = []
-for i in range(6):
+for i in range(len(data_not)):
     if (i % 2) == 0 :
         labels.append(1)
     else :
         labels.append(0)
-features = data_not.values
-scaler = StandardScaler()
+
 labels = np.array(labels)
-features3dim = [[0, 1], [5, 6], [10, 11], [15, 16], [20, 21], [25, 26]]
-j = 1
-l = 0
-"""
-for i in range(2):
-    if i % 150 == 0:
-        l = l + 1
-    features3dim.append()
-    j = j + 1
-    if j % 150 == 0:
-        j = 1
-"""
 
-features3dim = np.array(features3dim)
-print(features3dim.shape)
+labels.shape
 
+features = data_not.values
+
+features.shape
+
+type(features)
+
+# Standarisation des input.
+scaler = StandardScaler()
+features = scaler.fit_transform(features)
+
+features.shape
+
+features.mean(axis = 0)
+
+features.std(axis = 0)
+
+features3d = features.reshape(645261 , 1 , 8)
+
+features3d.shape
+
+# L'architecture de notre model.
 model = tf.keras.Sequential([
-    keras.layers.Embedding(input_dim=300, output_dim=64),
-    keras.layers.GRU(256, return_sequences=True),
+    keras.layers.GRU(256, return_sequences=True , input_dim = 8),
     keras.layers.SimpleRNN(128),
     keras.layers.Dense(1)
 ])
 
 model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam())
 
-model.fit(features3dim, labels, epochs=2, batch_size=1)
-print(model.output_shape)
+model.fit(features3d, labels, epochs=2, batch_size=32)
+
+"""**Commen on peut le voir, on a un MSE de 0.2503, afin d'augmenter les performances de notre système on peut:**
+
+*   Complixifier l'architecture de notre réseau de neuronnes.
+*   Augmenter le nombre d'épochs.
+
+
+
+      
+"""
+ 
