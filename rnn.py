440a174c86a4f4ac339d478c93c0fb8844e0d738
lucas101199 <lucas_fernandez@orange.fr>
2021-04-15 14:39:03 +0000

    add some comments

--- rnn.py
+++ rnn.py
@@ -8,28 +8,29 @@ from keras.utils.np_utils import to_categorical
 from sklearn.model_selection import train_test_split
 
 
+# Create a dataset from a DataFrame of size [samples, timesteps, features] and size [samples, action] for the label
 def CreateDataset(TimeSteps, df):
     size = math.ceil(len(df) / TimeSteps)
-    array_axis = np.empty([size, TimeSteps, 6])
+    features = np.empty([size, TimeSteps, 6])
     j = 0
     label = []
     for i in range(0, len(df), TimeSteps):
-        array_axis[j] = df.loc[i:i + TimeSteps - 1,
-                        ['X-AxisA', 'Y-AxisA', 'Z-AxisA', 'X-AxisG', 'Y-AxisG', 'Z-AxisG']].values
+        features[j] = df.loc[i:i + TimeSteps - 1,
+                      ['X-AxisA', 'Y-AxisA', 'Z-AxisA', 'X-AxisG', 'Y-AxisG', 'Z-AxisG']].values
         action = df.loc[i:i + TimeSteps - 1, ['Action']].values
         action = action.flatten()
         label.append(np.bincount(action).argmax())
         j += 1
-    return array_axis, label
+    return features, label
 
 
 df = pd.read_csv('AllData.csv', sep=';', header=0)
 
-array, label = CreateDataset(60, df)
+features, label = CreateDataset(60, df)
 label = np.array(label)
-label = np.reshape(label, (label.size, 1))
+label = np.reshape(label, (label.size, 1))  # reshape the array from [label] (3540,) to [samples, label] (3540,1)
 
-X_train, X_test, y_train, y_test = train_test_split(array, label, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=42)
 
 # zero-offset class values
 y_train = y_train - 1
@@ -51,4 +52,3 @@ test_loss, test_acc = model.evaluate(X_test, y_test)
 
 print('Test Loss:', test_loss)
 print('Test Accuracy:', test_acc)
-
