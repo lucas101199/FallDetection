f7deeaf85226bb5c1b7264adce272c9660119740
sidathgueye <sidathgueye@gmail.com>
2021-04-20 13:52:33 +0000

    plot de la partie summarize history for loss

--- rnn.py
+++ rnn.py
@@ -82,3 +82,13 @@ plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
+
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
