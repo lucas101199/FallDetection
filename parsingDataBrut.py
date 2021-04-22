31202bced9cc51987446d7607a2011fc6a94aac5
lucas101199 <lucas_fernandez@orange.fr>
2021-04-22 15:03:31 +0000

    parsing on own data

--- parsingDataBrut.py
+++ parsingDataBrut.py
@@ -4,14 +4,14 @@ lines = file.readlines()
 features = 'TimeStamp,X-Axis,Y-Axis,Z-Axis,Velocity\n'
 fileToWrite.write(features)
 lines_split_space = lines[0].split(' ')
-firstLine = lines_split_space[0] + ' ' + lines_split_space[1] + ','
+firstLine = lines_split_space[1] + ','
 fileToWrite.write(firstLine)
 del lines_split_space[0]
 del lines_split_space[0]
 for i in range(len(lines_split_space)):
     line_split = lines_split_space[i].split(";")
     if len(line_split) == 1:
-        fileToWrite.write(' ' + line_split[0] + ',')
+        fileToWrite.write(line_split[0] + ',')
     else:
         fet = line_split[:-1]
         allfeat = ''
@@ -19,4 +19,3 @@ for i in range(len(lines_split_space)):
             data = data.replace(',', '.')
             allfeat += data + ','
         fileToWrite.write(allfeat[:-1] + '\n')
-        fileToWrite.write(line_split[-1])
