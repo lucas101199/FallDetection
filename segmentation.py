58fd10cb2e204b87c80d90fd995512fb81b7869c
lucas101199 <lucas_fernandez@orange.fr>
2021-04-26 16:27:30 +0000

    create slicing window with the given time argument

--- segmentation.py
+++ segmentation.py
@@ -1,4 +1,23 @@
 # sliding windows of 2 sec and 50% overlap
-# time pass in argument is in second
-#def sliding_window(time, file):
+# time pass in argument is in second and return a set of windows
+import pandas as pd
 
+
+def sliding_window(file, time):
+    df = pd.read_csv(file)
+    df['TimeStamp'] = pd.to_datetime(df['TimeStamp'], infer_datetime_format=True)
+    timestamp = df['TimeStamp'].values
+    set_windows = []
+    print(timestamp[0])
+    start_time = timestamp[0]
+    end_time = timestamp[-1]
+    print(end_time)
+    while start_time + pd.offsets.Second(time) <= end_time + pd.offsets.Second(1):
+        data = df.loc[(df['TimeStamp'] >= start_time) & (df['TimeStamp'] <= start_time + pd.offsets.Second(time))]
+        start_time += pd.offsets.Second(time/2)
+        print(data)
+
+
+sliding_window('NotFAll/eponge1.csv', 2)
+
+#192.168.0.41
