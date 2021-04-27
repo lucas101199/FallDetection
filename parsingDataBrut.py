c6b847d4d9792866bbbada12eda3d0af98395f09
lucas101199 <lucas_fernandez@orange.fr>
2021-04-27 09:02:47 +0000

    parsing for the new data

--- parsingDataBrut.py
+++ parsingDataBrut.py
@@ -7,21 +7,9 @@ file_write = sys.argv[2]
 file = open(file_name, 'r')
 fileToWrite = open(file_write, 'w')
 lines = file.readlines()
-features = 'TimeStamp,X-Axis,Y-Axis,Z-Axis,Velocity\n'
+features = 'TimeStamp,X-Axis,Y-Axis,Z-Axis\n'
 fileToWrite.write(features)
-lines_split_space = lines[0].split(' ')
-firstLine = lines_split_space[1] + ','
-fileToWrite.write(firstLine)
-del lines_split_space[0]
-del lines_split_space[0]
-for i in range(len(lines_split_space)):
-    line_split = lines_split_space[i].split(";")
-    if len(line_split) == 1:
-        fileToWrite.write(line_split[0] + ',')
-    else:
-        fet = line_split[:-1]
-        allfeat = ''
-        for data in fet:
-            data = data.replace(',', '.')
-            allfeat += data + ','
-        fileToWrite.write(allfeat[:-1] + '\n')
+for line in lines:
+    line_split = line.split(';')
+    linetowrite = line_split[3][:-1] + ',' + line_split[0] + ',' + line_split[1] + ',' + line_split[2] + '\n'
+    fileToWrite.write(linetowrite)
