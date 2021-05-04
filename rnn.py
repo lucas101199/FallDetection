cea9f7e26dbbdae07271352449335733fb81b3d3
lucas101199 <lucas_fernandez@orange.fr>
2021-05-04 09:48:35 +0000

    add new data

--- rnn.py
+++ rnn.py
@@ -1,7 +1,5 @@
 import math
 import numpy as np
-
-"""
 import pandas as pd
 import tensorflow as tf
 from keras import Sequential
@@ -17,6 +15,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 
+"""
 # Create a dataset from a DataFrame of size [samples, timesteps, features] and size [samples, action] for the label
 def CreateDataset(TimeSteps, df):
     size = math.ceil(len(df) / TimeSteps)
@@ -37,7 +36,7 @@ def CreateDataset(TimeSteps, df):
 def extractLabel(lines):
     label = []
     for line in lines:
-        label.append(line[-2])
+        label.append(int(line[-2]))
     return label
 
 
@@ -51,7 +50,7 @@ def extractData(lines):
             if len(xyz) == 3:
                 array.append(xyz)
                 xyz = []
-            xyz.append(linesplit[i])
+            xyz.append(float(linesplit[i]))
         data.append(array)
     return data
 
@@ -62,49 +61,43 @@ lines = np.array(lines)
 label = np.array(extractLabel(lines)).reshape(len(lines), 1)
 data = np.array(extractData(lines))
 
-"""
-label = np.reshape(label, (label.size, 1))  # reshape the array from [label] (3540,) to [samples, label] (3540,1)
+# label = np.reshape(label, (label.size, 1))  # reshape the array from [label] (3540,) to [samples, label] (3540,1)
 
 # Encode the label
-le = preprocessing.LabelEncoder()
-label = le.fit_transform(label)
+# le = preprocessing.LabelEncoder()
+# label = le.fit_transform(label)
 
 # Split the dataset between train and test
-X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.3, random_state=42)
 
 # show the size of each cut
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
-
+"""
 # zero-offset class values
 y_train = y_train - 1
 y_test = y_test - 1
 # one hot encode y
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
-
+"""
 # le model de la machine ss
 model = Sequential()
-model.add(LSTM(100, input_shape=(100, 9), return_sequences=True,
-               kernel_regularizer=tf.keras.regularizers.L2(l2=0.01),
-               recurrent_regularizer=tf.keras.regularizers.L2(l2=0.01),
-               bias_regularizer=tf.keras.regularizers.L2(l2=0.01),
-               activity_regularizer=tf.keras.regularizers.L2(l2=0.01)))
-#model.add(Bidirectional(LSTM(32)))
-#model.add(Dense(100, activation='relu'))
-#model.add(Dense(12, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.add(LSTM(100, input_shape=(200, 3), return_sequences=True))
+model.add(Bidirectional(LSTM(32)))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # model.fit(X_train, y_train, epochs=20, batch_size=40)
 # modification of fit model
-history = model.fit(X_train, y_train, epochs=20, batch_size=16, validation_data=(X_test, y_test))
+history = model.fit(X_train, y_train, epochs=20, batch_size=4)
 
 # model.fit(X_train, y_train, epochs=20, batch_size=4)
 
-model.evaluate(X_test, y_test, batch_size=4)
+model.evaluate(X_test, y_test, batch_size=2)
 
 # list all data in history
-print(history.history.keys())
-
+# print(history.history.keys())
+"""
 # plot the training and validation accuracy and loss at each epoch
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
